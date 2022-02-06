67e1ef3156a867b0b6bae3a325ab5785a737e5ac
katusiky <josedeleon.ka@gmail.com>
2022-02-05 23:18:48 -0300

    Fin sección 7

--- lib/pages/usuarios.dart
+++ lib/pages/usuarios.dart
@@ -1,12 +1,105 @@
+import 'package:app/models/usuario.dart';
 import 'package:flutter/material.dart';
+import 'package:pull_to_refresh/pull_to_refresh.dart';
 
-class UsuariosPage extends StatelessWidget {
+class UsuariosPage extends StatefulWidget {
+  @override
+  State<UsuariosPage> createState() => _UsuariosPageState();
+}
+
+class _UsuariosPageState extends State<UsuariosPage> {
+  final usuarios = [
+    Usuario(uid: '1', nombre: 'João', email: 'test1@gmail.com', online: true),
+    Usuario(
+        uid: '2', nombre: 'Irindiana', email: 'test2@gmail.com', online: false),
+    Usuario(
+        uid: '3', nombre: 'Barbara', email: 'test3@gmail.com', online: true),
+    Usuario(uid: '4', nombre: 'Mama', email: 'test4@gmail.com', online: true),
+  ];
+  final _refreshController = RefreshController(initialRefresh: false);
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('UsuariosPage'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Mi Nombre',
+          style: TextStyle(
+            color: Colors.black87,
+          ),
+        ),
+        elevation: 1,
+        backgroundColor: Colors.white,
+        leading: IconButton(
+          icon: const Icon(
+            Icons.exit_to_app,
+            color: Colors.black87,
+          ),
+          onPressed: () {},
+        ),
+        actions: [
+          Container(
+            margin: const EdgeInsets.only(right: 10),
+            child: Icon(
+              Icons.check_circle,
+              color: Colors.blue[600],
+            ),
+            // const Icon(
+            //   Icons.offline_bolt,
+            //   color: Colors.red,
+            // ),
+          )
+        ],
+      ),
+      body: SmartRefresher(
+        controller: _refreshController,
+        header: WaterDropHeader(
+          complete: Icon(
+            Icons.check,
+            color: Colors.blue[400],
+          ),
+          waterDropColor: Colors.blue[400]!,
+        ),
+        onRefresh: _cargarUsuarios,
+        child: _listViewUsuarios(),
       ),
     );
+  }
+
+  ListView _listViewUsuarios() {
+    return ListView.separated(
+      physics: const BouncingScrollPhysics(),
+      itemBuilder: (_, i) => _usuarioListTile(usuarios[i]),
+      separatorBuilder: (_, i) => const Divider(),
+      itemCount: usuarios.length,
+    );
+  }
+
+  ListTile _usuarioListTile(Usuario usuario) {
+    return ListTile(
+      title: Text(usuario.nombre),
+      subtitle: Text(usuario.email),
+      leading: CircleAvatar(
+        backgroundColor: Colors.blue[100],
+        child: Text(
+          usuario.nombre.substring(0, 2),
+        ),
+      ),
+      trailing: Container(
+        width: 10,
+        height: 10,
+        decoration: BoxDecoration(
+          color: usuario.online ? Colors.green[300] : Colors.red,
+          borderRadius: BorderRadius.circular(100),
+        ),
+      ),
+    );
+  }
+
+  _cargarUsuarios() async {
+    // monitor network fetch
+    await Future.delayed(const Duration(milliseconds: 1000));
+    // if failed,use refreshFailed()
+    _refreshController.refreshCompleted();
   }
 }
---
+++ lib/widgets/chat_message.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+class ChatMessage extends StatelessWidget {
+  final String texto;
+  final String uid;
+  final AnimationController animationController;
+
+  const ChatMessage({
+    Key? key,
+    required this.texto,
+    required this.uid,
+    required this.animationController,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return FadeTransition(
+      opacity: animationController,
+      child: SizeTransition(
+        sizeFactor: CurvedAnimation(
+          parent: animationController,
+          curve: Curves.easeOut,
+        ),
+        child: Container(
+          child: uid == "123" ? _myMessage() : _notMyMessage(),
+        ),
+      ),
+    );
+  }
+
+  Widget _myMessage() {
+    return Align(
+      alignment: Alignment.centerRight,
+      child: Container(
+        decoration: BoxDecoration(
+          color: const Color(0xff4d9ef6),
+          borderRadius: BorderRadius.circular(20),
+        ),
+        padding: const EdgeInsets.all(8.0),
+        margin: const EdgeInsets.only(
+          bottom: 5,
+          left: 50,
+          right: 5,
+        ),
+        child: Text(
+          texto,
+          style: const TextStyle(color: Colors.white),
+        ),
+      ),
+    );
+  }
+
+  Widget _notMyMessage() {
+    return Align(
+      alignment: Alignment.centerLeft,
+      child: Container(
+        decoration: BoxDecoration(
+          color: const Color(0xffe4e5e8),
+          borderRadius: BorderRadius.circular(20),
+        ),
+        padding: const EdgeInsets.all(8.0),
+        margin: const EdgeInsets.only(
+          bottom: 5,
+          left: 5,
+          right: 50,
+        ),
+        child: Text(
+          texto,
+          style: const TextStyle(color: Colors.black87),
+        ),
+      ),
+    );
+  }
+}
